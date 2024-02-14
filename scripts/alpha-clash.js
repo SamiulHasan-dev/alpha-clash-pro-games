668b52e28567dab63a0d35c121293f2ad51c449f
SamiulHasan-dev <samiul.199897@gmail.com>
2024-02-14 14:12:19 +0600

    work to added and remove section with js

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,12 @@
+// function play(){
+//     //step 1: hide the home screen  to hide the screen add the class hidden to the home section
+
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList)
+
+//     //show the playground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+//     // console.log(playGround.classList)
+// }

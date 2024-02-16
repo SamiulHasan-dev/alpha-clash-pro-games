b519107aa98f43c46845105e86be1f0e527d008a
SamiulHasan-dev <samiul.199897@gmail.com>
2024-02-16 12:02:18 +0600

    match aplabet property

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,9 +1,33 @@
+
+function handleKeyboardKeyupEvent(event){
+    const playerPress = event.key;
+    console.log('player press:',playerPress);
+
+    //get the expected to press
+    const currentAlphabetElements = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElements.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPress, expectedAlphabet);
+
+    // check match or not
+    if(playerPress === expectedAlphabet){
+        console.log('get a point');
+    }
+    else{
+        console.log('you missed point');
+    }
+}
+
+
+document.addEventListener('keyup',handleKeyboardKeyupEvent);
+
 function continueGame(){
     //step 1: generate a random alphabet
     const  alphabet = getARandomAlphabet();
+    console.log('Your Random Alphabet',alphabet);
 
     //randomly alphabet showing in screen(show it)
-    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabetElement = document.getElementById('current-alphabet'); 
     currentAlphabetElement.innerText = alphabet;
 
     setBackgroundColorById(alphabet);
